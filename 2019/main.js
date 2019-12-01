7154d0d72c04eb082929582c51b9681bacdcf752
Karel Jakubec <karel.jakubec@vistaprint.com>
2019-12-01 13:41:40 +0100

    Add common stuff

---
+++ 2019/main.js
@@ -0,0 +1,37 @@
+
+const yargs = require('yargs');
+
+console.log("START")
+
+const argv = yargs
+    .option('solution', {
+        alias: 's',
+        description: 'Which solution should be used',
+        choices: ['hard', 'easy', 'e', 'h']
+    })
+    .option('day', {
+        alias: 'd',
+        description: 'Which day to solve',
+        type: 'integer'
+    })
+    .option('test', {
+        alias: 't',
+        description: 'Use test data',
+        type: 'boolean',
+        default: false
+    })
+    .demandOption(['day', 'solution'])
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+const handler = require("./" + argv.day + "/solution.js")
+
+if(argv.solution == "easy") {
+    console.log(handler.easy(argv.test));
+}
+else if (argv.solution == "hard") {
+    console.log(handler.hard(argv.test));
+}
+
+
---
+++ 2019/solution-template.js
@@ -0,0 +1,10 @@
+const easy = (test) => {
+    return "EASY_NOT_IMPLEMENTED"
+}
+
+const hard = (test) => {
+    return "HARD_NOT_IMPLEMENTED"
+}
+
+exports.easy = easy;
+exports.hard = hard;
