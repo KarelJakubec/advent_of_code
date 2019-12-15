8eaf3317d33808502810864170ce4b6129a6c35a
Karel Jakubec <karel.jakubec@vistaprint.com>
2019-12-15 09:58:45 +0100

    Put machine into separate file

--- 2019/2/solution.js
+++ 2019/2/solution.js
@@ -1,36 +1,18 @@
 const common = require("../common");
+const machine = require("../machine")
 
 const patchArray = (arr, val1, val2) => {
     arr[1] = val1;
     arr[2] = val2;
 }
 
-const runMachine = (data) => {
-    let pos = 0;
-    while(data[pos] != 99) {
-        let opcode = data[pos]
-        let pos1 = data[pos + 1];
-        let pos2 = data[pos + 2];
-        let store_pos = data[pos + 3];
 
-        if(opcode == 1) {
-            data[store_pos] = data[pos1] + data[pos2];
-        }
-        else if(opcode == 2) {
-            data[store_pos] = data[pos1] * data[pos2];
-        }
-        else {
-            console.log("ERROR " + pos + " " + data[pos]);
-        }
-        pos += 4;
-    }
-    return data[0];
-}
 
 const easy = (test) => {
     let data = common.readToArray('./2/data')[0].split(',').map(x => parseInt(x));
     patchArray(data, 12, 2);
-    return runMachine(data);
+    machine.runMachine(data, () => 0);
+    return data[0]
 }
 
 const hard = (test) => {
@@ -40,7 +22,8 @@ const hard = (test) => {
         for(let j = 0; j < 100; ++j) {
             let data = common.readToArray('./2/data')[0].split(',').map(x => parseInt(x));
             patchArray(data, i, j);
-            if(runMachine(data) === TARGET) {
+            machine.runMachine(data, () => 0)
+            if(data[0] === TARGET) {
                 return 100 * i + j;
             }
         }
